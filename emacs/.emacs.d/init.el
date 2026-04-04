0c6760c5e8d0cfc289023f6004a158de5c533810
Tomas Fabrizio Orsi <torsi@fi.uba.ar>
2026-04-04 11:51:47 -0300

    feat: size
    Signed-off-by: Tomas Fabrizio Orsi <torsi@fi.uba.ar>

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -34,7 +34,10 @@
 ;; Font
 (if (equal fabri-profile 'personal)
     (add-to-list 'default-frame-alist
-                 '(font . "-1ASC-Liberation Mono-regular-normal-normal-*-16-*-*-*-m-0-iso10646-1"))
+                 (cons 'font (font-xlfd-name
+                              (font-spec
+                               :family "DejaVu Sans Mono"
+                               :size 16))))
   (set-face-attribute 'default nil :height 160)
   )
 
@@ -842,19 +845,3 @@
   ;;; Enable dashboard-dired
   (add-hook 'dired-mode-hook 'all-the-icons-dired-mode)
   )
-
-
-
-
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages nil))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
